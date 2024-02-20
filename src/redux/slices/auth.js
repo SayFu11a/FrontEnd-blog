16d4aec64422ff7f5ea872c0cb2765808fc8b693
SayFu11a <sgishlakaev@mail.ru>
2024-02-20 20:26:31 +0300

    add authtorization

--- src/redux/slices/auth.js
+++ src/redux/slices/auth.js
@@ -1,8 +1,59 @@
-// import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from '../../axios';
 
-// const initialState = {
-//    data: null,
-//    status: 'loading'
-// };
+export const fetchUserData = createAsyncThunk('auth/fetchUserData', async (params) => {
+   const { data } = await axios.post('/auth/login', params);
+   return data;
+});
 
-// const
+export const fetchAuthMe = createAsyncThunk('auth/fetchAuthMe', async () => {
+   const { data } = await axios.get('/auth/me');
+   return data;
+});
+
+const initialState = {
+   data: null,
+   status: 'loading',
+};
+
+const authSlice = createSlice({
+   name: 'auth',
+   initialState,
+   reducers: {
+      logout: (state) => {
+         state.data = null;
+      },
+   },
+   extraReducers: {
+      [fetchUserData.pending]: (state) => {
+         state.status = 'loading';
+         state.data = null;
+      },
+      [fetchUserData.fulfilled]: (state, action) => {
+         state.status = 'loaded';
+         state.data = action.payload;
+      },
+      [fetchUserData.rejected]: (state) => {
+         state.status = 'error';
+         state.data = null;
+      },
+      [fetchAuthMe.pending]: (state) => {
+         state.status = 'loading';
+         state.data = null;
+      },
+      [fetchAuthMe.fulfilled]: (state, action) => {
+         state.status = 'loaded';
+         state.data = action.payload;
+      },
+      [fetchAuthMe.rejected]: (state) => {
+         state.status = 'error';
+         state.data = null;
+      },
+   },
+});
+
+export const selectIsAuth = (state) => Boolean(state.auth.data);
+
+export const authReducer = authSlice.reducer;
+
+export const { logout } = authSlice.actions;
