22062f856e2a3540bd5b99dc4331142c381b1582
SayFu11a <sgishlakaev@mail.ru>
2024-02-24 00:13:30 +0300

    fix Url

--- src/axios copy.js
+++ src/axios copy.js
@@ -50,7 +50,7 @@ export const AddPost = () => {
          const fields = {
             title,
             imageUrl,
-            tags,
+            tags: tags.split(','),
             text,
          };
 
