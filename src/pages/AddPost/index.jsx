ad8f643990fe3200e89270889c62cff73203b28b
SayFu11a <sgishlakaev@mail.ru>
2024-02-24 14:10:06 +0300

    fix version work

--- src/pages/AddPost/index.jsx
+++ src/pages/AddPost/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { useNavigate, Navigate } from 'react-router-dom';
+import { useNavigate, Navigate, useParams } from 'react-router-dom';
 
 import TextField from '@mui/material/TextField';
 import Paper from '@mui/material/Paper';
@@ -13,6 +13,7 @@ import axios from '../../axios';
 import styles from './AddPost.module.scss';
 
 export const AddPost = () => {
+   const { id } = useParams();
    const navigate = useNavigate();
    const isAuth = useSelector(selectIsAuth);
    const [isLoading, setLoading] = React.useState(false);
@@ -21,6 +22,8 @@ export const AddPost = () => {
    const [tags, setTags] = React.useState('');
    const [imageUrl, setImageUrl] = React.useState('');
    const inputFileRef = React.useRef(null);
+
+   const isEditing = Boolean(id);
 
    const handleChangeFile = async (event) => {
       try {
@@ -54,16 +57,35 @@ export const AddPost = () => {
             text,
          };
 
-         const { data } = await axios.post('/posts', fields);
+         const { data } = isEditing
+            ? await axios.patch(`/posts/${id}`, fields)
+            : await axios.post('/posts', fields);
 
-         const id = data._id;
+         const _id = isEditing ? id : data._id;
 
-         navigate(`/posts/${id}`);
+         navigate(`/posts/${_id}`);
       } catch (err) {
          console.warn(err);
          alert('ошибка при создании статьи!11');
       }
    };
+
+   React.useEffect(() => {
+      if (id) {
+         axios
+            .get(`/posts/${id}`)
+            .then(({ data }) => {
+               setTitle(data.title);
+               setText(data.text);
+               setImageUrl(data.imageUrl);
+               setTags(data.tags.join(','));
+            })
+            .catch((err) => {
+               console.warn(err);
+               alert('ошибка при получении статьи!11');
+            });
+      }
+   }, []);
 
    const options = React.useMemo(
       () => ({
@@ -124,7 +146,7 @@ export const AddPost = () => {
          <SimpleMDE className={styles.editor} value={text} onChange={onChange} options={options} />
          <div className={styles.buttons}>
             <Button onClick={onSubmit} size="large" variant="contained">
-               Опубликовать
+               {isEditing ? 'Редактировать' : 'Опубликовать'}
             </Button>
             <a href="/">
                <Button size="large">Отмена</Button>
